e1bb56e37b7f42cf061861003d31df4634aef573
luvsunlight <516060659@qq.com>
2020-09-17 16:12:35 +0800

    chore:init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import './App.less';
+import React from "react"
+import { APP_CONFIG } from "./config"
+import "./App.less"
 
 function App() {
-  return <div className="app">app</div>
+    return (
+        <div className="app">
+            <div className="app-header">XLab实验室项目展示</div>
+            <div className="app-content">
+                {APP_CONFIG.map((app) => {
+                    return <div className="app-content-item">{app.name}</div>
+                })}
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const APP_CONFIG = [
+    {
+        name: "smart-sensitive-word",
+        url: "https://luvsunlight.github.io/smart-sensitive-word/",
+    },
+]
