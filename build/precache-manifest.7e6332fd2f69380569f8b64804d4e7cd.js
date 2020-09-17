e679b8bf129c965e72b06d9ed13b9ac92f9795e4
luvsunlight <516060659@qq.com>
2020-09-17 16:31:03 +0800

    fix:add homepage field in package.json

---
+++ build/precache-manifest.7e6332fd2f69380569f8b64804d4e7cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396e99c50b77f1170c27a92ac500479f",
+    "url": "/gallery/index.html"
+  },
+  {
+    "revision": "1176c1c94a561c5edc21",
+    "url": "/gallery/static/css/main.90ac1ecd.chunk.css"
+  },
+  {
+    "revision": "f17eaec48e564cf09bb7",
+    "url": "/gallery/static/js/2.c6f147ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gallery/static/js/2.c6f147ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1176c1c94a561c5edc21",
+    "url": "/gallery/static/js/main.69ebeb7b.chunk.js"
+  },
+  {
+    "revision": "1160e6796d671babbaaa",
+    "url": "/gallery/static/js/runtime-main.7b22ee28.js"
+  }
+]);
