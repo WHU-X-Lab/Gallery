480cfd1fc458addb29d792c6d34484bf305188c6
luvsunlight <516060659@qq.com>
2020-09-17 16:21:45 +0800

    chore:build

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Gallery
-A gallery to display demos
+
+> X-Lab 实验室工作汇总，部分有线上 DEMO
+
+# Contribution
+
+```bash
+git clone git@github.com:WHU-X-Lab/gallery.git
+
+npm i
+
+# dev
+
+npm start
+
+# publish
+
+npm run publish
+```
---
+++ docs/precache-manifest.79ad1bc3415d067d2d3cb0d3fa54299e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2669018dd4a1c405927a6701d8a5c5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "449e9964c7bf5b6cac16",
+    "url": "/static/css/main.90ac1ecd.chunk.css"
+  },
+  {
+    "revision": "f17eaec48e564cf09bb7",
+    "url": "/static/js/2.c6f147ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6f147ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449e9964c7bf5b6cac16",
+    "url": "/static/js/main.a4894d52.chunk.js"
+  },
+  {
+    "revision": "6df8defb7fac4f8ad5c4",
+    "url": "/static/js/runtime-main.02737e24.js"
+  }
+]);
