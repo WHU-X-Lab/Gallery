3a834b1656ba9f64b82a028495f19329df07a831
luvsunlight <516060659@qq.com>
2020-09-28 14:33:17 +0800

    feat:增加3D地球的配置

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 export const APP_CONFIG = [
     {
-        name: "smart-sensitive-word",
-        url: "https://luvsunlight.github.io/smart-sensitive-word/",
-        image:
-            "https://tva1.sinaimg.cn/large/007S8ZIlly1gj1fqlf8t8j31q10u0jv8.jpg",
-    },
-    {
         name: "风场可视化",
         url: "https://whu-x-lab.github.io/wind-flow/",
         image:
             "https://tva1.sinaimg.cn/large/007S8ZIlly1gj1fifehuwj31q90u0kjo.jpg",
+    },
+    {
+        name: "3D地球",
+        url: "https://whu-x-lab.github.io/3d-earth/",
+        image:
+            "https://tva1.sinaimg.cn/large/007S8ZIlgy1gj6c0t107ij31qc0u0dq6.jpg",
     },
 ]
